87a5c42135d6c21578601ecb5a2279723e2d0290
kyjita <kyjita@gmail.com>
2024-04-23 18:24:20 +0800

    修改keywords 增加time limit

--- src/main.py
+++ src/main.py
@@ -1,41 +1,9 @@
 
-from media.ettoday import EtToday
+from src.media.ettoday import EtToday
 
 
-def main():
-
-    keywords = [
-        '新北市板橋',
-        '新北市三重',
-        '新北市中和',
-        '新北市永和',
-        '新北市新莊',
-        '新北市新店',
-        '新北市土城',
-        '新北市蘆洲',
-        '新北市樹林',
-        '新北市汐止',
-        '新北市鶯歌',
-        '新北市三峽',
-        '新北市淡水',
-        '新北市瑞芳',
-        '新北市五股',
-        '新北市泰山',
-        '新北市林口',
-        '新北市深坑',
-        '新北市石碇',
-        '新北市坪林',
-        '新北市三芝',
-        '新北市石門',
-        '新北市八里',
-        '新北市平溪',
-        '新北市雙溪',
-        '新北市貢寮',
-        '新北市金山',
-        '新北市萬里',
-        '新北市烏來',
-    ]
-    ettoday = EtToday()
+def main(keywords = None, weeks_limit = None):
+    ettoday = EtToday(weeks_limit)
     for keyword in keywords:
         ettoday.search(keyword)
 
--- src/media/ettoday.py
+++ src/media/ettoday.py
@@ -1,10 +1,16 @@
 import bs4, requests, time
-from recorder import to_txt_record
+from src.recorder import to_txt_record
+from datetime import datetime, timedelta
 class EtToday:
     keyword = None
 
     url = "https://www.ettoday.net/news_search/doSearch.php?search_term_string="
     soup = None
+    reach_end = False
+    week_limit = None
+    def __init__(self,week_limit=None):
+        if week_limit is not None:
+            self.week_limit = timedelta(weeks=week_limit) # 這是一個timedelta物件
 
     def search(self, keyword=None):
         print('ETtoday search: ' + keyword)
@@ -17,6 +23,8 @@ class EtToday:
         # 範例 第1頁 | 共1000頁
         last_page = int(page_info.split('|')[1].split('共')[1].replace('頁', ''))
         for i in range(1, last_page + 1):
+            if self.reach_end:
+                break
             self.parse_page(i)
     def parse_page(self, page):
         if self.keyword is None:
@@ -41,7 +49,20 @@ class EtToday:
         time_selector = 'time.date'
         title = soup.select(title_selector)[0].text.strip()
         content = soup.select(content_selector)
-        time = soup.select(time_selector)[0].text.strip()
+        time = soup.select(time_selector)[0].text.strip() # 格式 2022年01月26日 07:15
+        print(time)
+        print(self.week_limit )
+        if self.week_limit is not None:
+            # 把time轉換成能比較的格式
+            time = time.split(' ')
+            time = time[0].replace('年', '-').replace('月', '-').replace('日', ' ') + time[1]
+            time = time.strip()
+            time = time.replace(' ', 'T')
+            time = time + ':00'
+            time = datetime.fromisoformat(time)
+            if datetime.now() - time > self.week_limit:
+                self.reach_end = True
+                return
         # 移除time的空白 
         time = time.strip()
         contentText = ''
@@ -51,9 +72,8 @@ class EtToday:
         for c in content:
             if c.text == '':
                 continue
-            # 如果 c.text 是 ►、●、▲ 這三個符號開頭的話，就不要印出來
-            if c.text.startswith('►') or c.text.startswith('●') or c.text.startswith('▲'):
-                continue
+            if c.text.startswith('►') or c.text.startswith('●') or c.text.startswith('▲') or c.text.startswith('▼') or c.text.startswith('▸'):
+                continue 
             if author == '':
                 author = c.text
             contentText += c.text + '\n'
